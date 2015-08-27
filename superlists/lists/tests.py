abc061f822bc3d95552ca5c76dff598d28418a24
Paolo Anchoriz <paolo.anchoriz@gmail.com>
2015-08-27 22:12:40 +0800

    Refactor for simpler tests, added templates, added app to settings

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,6 +1,7 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 from lists.views import home_page
 
@@ -13,8 +14,5 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        content = response.content
-        print(content)
-        self.assertTrue(content.startswith(b'<html>'))
-        self.assertIn(b'<title>To-Do lists</title>', content)
-        self.assertTrue(content.endswith(b'</html>'))
+        expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_html)
